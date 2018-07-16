6579c00649b7650aa5155a0c18ed09b7e2313a3b
User <User@User-PC>
2018-07-16 20:49:28 +0300

    Calculator update

--- lessons1-3/homework/calculate.php
+++ lessons1-3/homework/calculate.php
@@ -1,41 +1,43 @@
 <?php
 
-//TODO check keys
-
-$num1 = $_GET['num1'];
-$num2 = $_GET['num2'];
+if (isset($_GET["num1"]) && isset($_GET["num2"]) && isset($_GET["del"])) {
+    $num1 = $_GET['num1'];
+    $num2 = $_GET['num2'];
 
 //check is variable numeric
-if (!is_numeric($num1) || !is_numeric($num2)){
-    echo 'Wrong format';
-    exit;
-}
-if (urlencode($_GET['del'])== '+'){
-    $delimeter = urlencode($_GET['del']);
-} else {
-    $delimeter = $_GET['del'];
-}
+    if (!is_numeric($num1) || !is_numeric($num2)) {
+        echo 'Wrong format';
+        exit;
+    }
+    if (urlencode($_GET['del']) == '+') {
+        $delimeter = urlencode($_GET['del']);
+    } else {
+        $delimeter = $_GET['del'];
+    }
 
-switch ($delimeter){
-    case '+':
-        echo "$num1 + $num2 = ", $num1 + $num2;
-        break;
-    case '-':
-        echo "$num1 - $num2 = ", $num1 - $num2;
-        break;
-    case '/':
-        if($num2 == 0){
-            echo 'Division by zero is not cool';
+    switch ($delimeter) {
+        case '+':
+            echo "$num1 + $num2 = ", $num1 + $num2;
             break;
-        }
-        echo "$num1 / $num2 = ", $num1 / $num2;
-        break;  
-    case '*':
-        echo "$num1 * $num2 = ", $num1 * $num2;
-        break;
-    default:
-        echo "Wrong format";
-        break;
+        case '-':
+            echo "$num1 - $num2 = ", $num1 - $num2;
+            break;
+        case '/':
+            if ($num2 == 0) {
+                echo 'Division by zero is not cool';
+                break;
+            }
+            echo "$num1 / $num2 = ", $num1 / $num2;
+            break;
+        case '*':
+            echo "$num1 * $num2 = ", $num1 * $num2;
+            break;
+        default:
+            echo "Wrong parameters";
+            break;
+    }
+} else {
+    echo 'error';
 }
 
 
